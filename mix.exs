2e976b5be9337361e5fb342704f2f30f9a037817
Dennis Ideler <ideler.dennis@gmail.com>
2025-11-13 17:39:05 +0000

    Bump ex_doc to v0.39

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Money.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.38", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.39", only: :dev, runtime: false}
     ]
   end
 end
